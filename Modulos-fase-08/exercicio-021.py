6e57a8176ddfd2d5963a6278ee4bbadf2d4d744f
Leandro T. daSilva <leandro90ads@gmail.com>
2024-01-06 01:20:13 -0300

    Update exercicio-021.py

--- Modulos-fase-08/exercicio-021.py
+++ Modulos-fase-08/exercicio-021.py
@@ -2,8 +2,8 @@
     Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 No exercício original é usado o pacote pygame, porém não consegui instalar nem fazer rodar.
 
-    Para fazer este exercício optei por usar uma solução alternativa usando a bibliteca webbrowser
-como estou usando uma Distro Linux (Pop Os) ao executar o programa ele abre uma janela pedindo para escolher um programa
+    Para fazer este exercício optei por usar uma solução alternativa usando a bibliteca webbrowser,
+como estou usando uma Distro Linux (Pop Os), ao executar o programa ele abre uma janela pedindo para escolher um programa
 para reproduzir o audio no meu caso o VLC.
 
     Não testei em outros sistemas, mas acredito que no Windows/Mac Os dever abrir uma aba no navegador que reproduz o arquivo
