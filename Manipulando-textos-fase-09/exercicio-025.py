cd0e8d766c2b6a173c4ba28d400892538b1c36d9
Leandro T. daSilva <leandro90ads@gmail.com>
2024-01-08 18:30:46 -0300

    Create exercicio-025.py

---
+++ Manipulando-textos-fase-09/exercicio-025.py
@@ -0,0 +1,9 @@
+'''
+ Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+
+'''
+
+seuNome = str(input(' Digite seu nome completo \n')).strip()
+seuNome.find('Silva', 0)
+seuNome.capitalize()
+print('Seu Nome te Silva? {}'.format('Silva' in seuNome))
