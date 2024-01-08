263289ddf77017441122319748f5049ee5fbdd7d
Leandro T. daSilva <leandro90ads@gmail.com>
2024-01-08 20:47:33 -0300

    Update exercicio-026.py

--- Manipulando-textos-fase-09/exercicio-026.py
+++ Manipulando-textos-fase-09/exercicio-026.py
@@ -2,15 +2,12 @@
  Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
 
 
-Apenas observando que optei por usa o contagem do Python que leva em consideração O (zero)
 
+    *** Abaixo a versão original que leva em considerção a contagem do Python começando do 0 (zero)
 
-
-Abaixo segue o código para contagem normal da letras a partir da 1º letra
-
-print('A frase acima tem {} letras (a)'.format(fraseEntrada.count('a')))
-print('A primeira letra A esta na posição {}'.format(fraseEntrada.find('a', 0) + 1))
-print('A última letra A esta na posição {}'.format(fraseEntrada.rfind('a') + 1))
+    print('A frase acima tem {} letras (a)'.format(fraseEntrada.count('a')))
+    print('A primeira letra A esta na posição {}'.format(fraseEntrada.find('a', 0)))
+    print('A última letra A esta na posição {}'.format(fraseEntrada.rfind('a')))
 
 
 '''
@@ -18,7 +15,7 @@ print('A última letra A esta na posição {}'.format(fraseEntrada.rfind('a') + 
 fraseEntrada = str(input(' Digite uma frase curta \n')).strip()
 fraseEntrada = fraseEntrada.lower()
 print('A frase acima tem {} letras (a)'.format(fraseEntrada.count('a')))
-print('A primeira letra A esta na posição {}'.format(fraseEntrada.find('a', 0)))
-print('A última letra A esta na posição {}'.format(fraseEntrada.rfind('a')))
+print('A primeira letra A esta na posição {}'.format(fraseEntrada.find('a', 0) + 1))
+print('A última letra A esta na posição {}'.format(fraseEntrada.rfind('a') + 1))
 
 
