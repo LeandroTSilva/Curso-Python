c8ee30e89284be071b1e14f3556e9d27e4535a12
Leandro T. daSilva <leandro90ads@gmail.com>
2024-01-10 10:48:08 -0300

    Update exercicio-035.py

--- Condicionais-fase-10/exercicio-035.py
+++ Condicionais-fase-10/exercicio-035.py
@@ -1,14 +1,14 @@
 """
- Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+ Desenvolva um programa que leia o comprimento de três segmentos e diga ao usuário se elas podem ou não formar um triângulo.
 
 """
 
-reta1 = float(input('Digite a reta 1 \n'))
-reta2 = float(input('Digite a reta 2 \n'))
-reta3 = float(input('Digite a reta 3 \n'))
+segmento1 = float(input('Digite o segmento 1 \n'))
+segmento2 = float(input('Digite o segmento 2 \n'))
+segmento3 = float(input('Digite o segmento 3 \n'))
 
-if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
-    print('Com os valores {}, {} e {} é possível ter um triângulo'.format(reta1, reta2, reta3))
+if segmento1 < segmento2 + segmento3 and segmento2 < segmento1 + segmento3 and segmento3 < segmento1 + segmento2:
+    print('Com os valores {}, {} e {} é possível obter um triângulo'.format(segmento1, segmento2, segmento3))
 else:
-    print('Com os valores {}, {} e {} NÃO possível obter um triângulo'.format(reta1, reta2, reta3))
+    print('Com os valores {}, {} e {} NÃO é possível obter um triângulo'.format(segmento1, segmento2, segmento3))
 print(' **** Fim do Programa **** ')
