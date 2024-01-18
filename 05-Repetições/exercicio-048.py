5a391bfc8358362ae31864b45aeab942b1743d89
Leandro T. daSilva <leandro90ads@gmail.com>
2024-01-18 00:55:52 -0300

    Create exercicio-048.py

---
+++ 05-Repetições/exercicio-048.py
@@ -0,0 +1,15 @@
+"""
+ Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+Não consegui fazer este exercício, pois travei nos acumuladores, preciso treinar mais exercicios comos conceito de acumuladores
+
+
+"""
+
+total = 0
+cont = 0
+for c in range(1, 501, 2):
+   if c % 3 == 0:
+      total = total + c
+      cont = cont + 1
+print('A soma de todos os {} solicitados é {}'.format(cont, total))
